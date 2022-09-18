c90082f51790c3c3f80409757bc68638c7bb3a21
Bish <shortymonk@gmail.com>
2022-09-18 13:42:17 +0400

    added: - Grid Layout - Thumbnails - async loading of images - smooth appearance of loaded image

--- app/src/main/java/com/hfad/someapp/HomeFragment.kt
+++ app/src/main/java/com/hfad/someapp/HomeFragment.kt
@@ -12,9 +12,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.SmoothScroller
 
 class HomeFragment : Fragment() {
 
