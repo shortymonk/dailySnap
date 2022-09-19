021e0b596168cbb70c1adc295ffb6655ab581e52
Bish <shortymonk@gmail.com>
2022-09-19 16:27:03 +0400

    added: - Grid Layout - Thumbnails - async loading of images - smooth appearance of loaded image - previews are stored to cache

--- app/src/main/java/com/hfad/someapp/HomeFragment.kt
+++ app/src/main/java/com/hfad/someapp/HomeFragment.kt
@@ -1,15 +1,10 @@
 package com.hfad.someapp
 
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.media.MediaMetadataRetriever
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -17,24 +12,20 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.io.File
-import java.io.FileOutputStream
 
 const val IMAGE_CACHE = "/thumbnails"
 
 class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by viewModels()
-    lateinit var cacheDir: File
+    private lateinit var cacheDir: File
 
     companion object {
         const val PERMISSION_READ = android.Manifest.permission.READ_EXTERNAL_STORAGE
         const val PERMISSION_REQUEST_CODE = 2128
     }
 
-    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
